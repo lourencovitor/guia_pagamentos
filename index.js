cc181f47b299785290d04338de362b25886d21d1
Vitor Lourenço <50245199+lourencovitor@users.noreply.github.com>
2020-08-09 14:57:57 -0300

    adjusts

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 const express = require("express");
 const MercadoPago = require("mercadopago");
+const { setTimeout } = require("timers");
 const app = express();
 
 MercadoPago.configure({
   sandbox: true,
   access_token:
-    "TEST-6444911303132115-080819-211d97db361dcb2a6d1d651683a8bd5e-209776910",
+    "TEST-3260321011293596-061906-6cdf2c22206e79007f91b8f5642b3cf2-267336909",
 });
 
 app.get("/", (req, res) => {
-  res.send(`Olá mundo! ${Date.now()}`);
+  MercadoPago.payment
+    .search({
+      //qs: filters
+    })
+    .then(function (data) {
+      res.send(data);
+    })
+    .catch(function (error) {
+      res.send(error);
+    });
 });
 
-const id = `${Date.now()}`;
-const emailDoPagador = "vitor.brother17@gmail.com";
-
 app.get("/pagar", async (req, res) => {
-  const dados = {
+  // Pagamentos
+
+  // id // codigo // pagador // status
+  // 1 // 1593163315787 // victordevtb@gmail.com  // Não foi pago
+  // 2 //  1593163315782 // victordevtb2@gmail.com // Pago
+
+  var id = "" + Date.now();
+  var emailDoPagador = "victordevtb@outlook.com";
+
+  var dados = {
     items: [
       (item = {
-        id: id, // UUID && Data
-        title: "2x Video games",
+        id: id,
+        title: "2x video games;3x camisas",
         quantity: 1,
         currency_id: "BRL",
         unit_price: parseFloat(150),
@@ -29,21 +45,23 @@ app.get("/pagar", async (req, res) => {
     payer: {
       email: emailDoPagador,
     },
-    external_references: id,
+    external_reference: id,
   };
+
   try {
-    const pagamento = await MercadoPago.preferences.create(dados);
+    var pagamento = await MercadoPago.preferences.create(dados);
+    //Banco.SalvarPagamento({id: id, pagador: emailDoPagador});
     return res.redirect(pagamento.body.init_point);
   } catch (err) {
-    console.log(err);
     return res.send(err.message);
   }
 });
 
 app.post("/not", (req, res) => {
-  const { id } = req.body;
+  var id = req.query.id;
+
   setTimeout(() => {
-    const filtro = {
+    var filtro = {
       "order.id": id,
     };
 
@@ -52,7 +70,7 @@ app.post("/not", (req, res) => {
         qs: filtro,
       })
       .then((data) => {
-        const pagamento = data.body.results[0];
+        var pagamento = data.body.results[0];
 
         if (pagamento != undefined) {
           console.log(pagamento.external_reference);
